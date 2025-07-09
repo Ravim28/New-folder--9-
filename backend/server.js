0a80ca072d5a2973b59ccd4f8efc27f32dd4a0c5
Ravimalviya28 <mansharptech.ravi@gmail.com>
2025-07-09 11:48:35 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+ const app = express();
+
+
+ app.get('/get' ,(req, res) => {
+    res.send("hello world");
+ })
+ 
+ app.listen(3000 ,()=>{
+    console.log("Server is running on port 3000");
+ })
+
+
+
+ const a   =  new Promise ((resolve , reject) => {
+     // Change this to false to test rejection
+    if(true){
+        resolve("done")
+    }
+    else{
+        reject("error")
+    }
+ })
+ a.then((data)=>{
+    console.log(data);})
+    a.catch((err)=>{
+        console.log(err);
+    })
